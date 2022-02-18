e8b76867a79d75a0367e2ce5b57845f48a037afb
Tsio RJ <yyroist@gmail.com>
2022-02-18 18:18:08 +0100

    User - Affichage du détail d'une commande

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -13,7 +13,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/panier')]
 #[IsGranted('IS_AUTHENTICATED_FULLY')]
 class PanierController extends AbstractController
 {
@@ -25,7 +24,7 @@ class PanierController extends AbstractController
      * @return Response
      * @throws NonUniqueResultException
      */
-    #[Route('/', name: 'panier_current', methods: ['GET'])]
+    #[Route('/panier', name: 'panier_current', methods: ['GET'])]
     public function index(PanierRepository $repository, EntityManagerInterface $em): Response
     {
         // Récupération du dernier panier non payé de l'utilisateur
@@ -44,40 +43,17 @@ class PanierController extends AbstractController
         return $this->render('panier/index.html.twig', ['panier' => $panier]);
     }
 
-    #[Route('/{id}', name: 'panier_show', methods: ['GET'])]
+    /**
+     * Affichage du contenu d'une commande.
+     *
+     * @param Panier $panier
+     * @return Response
+     */
+    #[Route('/commande/{id}', name: 'panier_show', methods: ['GET'])]
     public function show(Panier $panier): Response
     {
         return $this->render('panier/show.html.twig', [
             'panier' => $panier,
         ]);
-    }
-
-    #[Route('/{id}/edit', name: 'panier_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Panier $panier, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(PanierType::class, $panier);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('panier_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('panier/edit.html.twig', [
-            'panier' => $panier,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'panier_delete', methods: ['POST'])]
-    public function delete(Request $request, Panier $panier, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$panier->getId(), $request->request->get('_token'))) {
-            $entityManager->remove($panier);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('panier_index', [], Response::HTTP_SEE_OTHER);
     }
 }
