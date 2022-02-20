2d819bba4ef2cf61ccd2e8e463b7f00709056570
Tsio RJ <yyroist@gmail.com>
2022-02-20 03:03:03 +0100

    Produit - Suppression automatique de la photo

--- src/Entity/Produit.php
+++ src/Entity/Produit.php
@@ -6,8 +6,12 @@ use App\Repository\ProduitRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\DependencyInjection\ParameterBag\ContainerBag;
+use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 #[ORM\Entity(repositoryClass: ProduitRepository::class)]
+#[ORM\HasLifecycleCallbacks]
 class Produit
 {
     #[ORM\Id]
@@ -131,5 +135,11 @@ class Produit
         }
 
         return $this;
+    }
+
+    #[ORM\PostRemove]
+    public function postRemove(): void
+    {
+        unlink(__DIR__.'/../../public/images/produit/' . $this->photo);
     }
 }
